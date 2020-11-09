d4b62457aa0056897b226fbf6778592f3b1d1ef9
aditmittal <45120598+aditmittal@users.noreply.github.com>
2020-11-09 15:35:41 +0530

    Add files via upload

---
+++ LAB 6/5b.r
@@ -0,0 +1,15 @@
+library(ggplot2)
+set.seed(100)
+x<-rnorm(100,mean=0,sd=1)
+x
+y<-rnorm(100,mean=0,sd=1)
+y
+dat<-as.data.frame(cbind(x,y))
+boxplot(dat, col="blue")
+
+x<-rnorm(100,mean=0,sd=1)
+x
+y<-rnorm(100,mean=0,sd=1)
+y
+dat<-as.data.frame(cbind(x,y))
+ggplot()+geom_point(data=dat, aes(x=x,y=y),size=3, color="blue")+ggtitle("Scatter Plot")+labs(y= "Y Axis", x = "X Axis")
